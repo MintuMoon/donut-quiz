7b1eb6f14b616f15587dcdd119eee2a7a3c35ab7
SlavikGong <slavikgongong@gmail.com>
2026-07-08 23:48:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
-// Global Configuration
+// Global settings config
 const CONFIG = {
-  QUIZ_LENGTH: 15,
-  SECRET_CHANCE: 0.03, 
-  STORAGE_KEY: "donut_quiz_history",
+    QUIZ_LENGTH: 20,                // Total active questions
+    SECRET_CHANCE: 0.03,            // Chance for the secret question (3%)
+    STORAGE_KEY: "donut_quiz_hist", // LocalStorage match history key
+    STATS_KEY: "donut_quiz_stats"   // LocalStorage personal counter key
 };
